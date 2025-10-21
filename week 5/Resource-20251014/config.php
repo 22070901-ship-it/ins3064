9413c91e15d53f7d1a8c8947601fbb1bfd7deca9
Nguyễn Quang Thái <22070901@vnu.edu.vn>
2025-10-21 14:22:06 +0700

    .

--- week 5/Resource-20251014/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "root", "123456", "login_demo");
-
-if (!$link) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
--- week 5/registration.php
+++ week 5/registration.php
@@ -3,7 +3,7 @@ session_start();
 header("location:login.php");
 /* connect to database check user*/
 $con=mysqli_connect('localhost','root');
-mysqli_select_db($con,LoginReg);
+mysqli_select_db($con,'LoginReg');
 
 /* create variables to store data */
 $name =$_POST['user'];
